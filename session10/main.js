67c578c4cb83fc130fdb71be6862cb15b8c2dfa0
mohamed amr <dev.mohamedamr@gmail.com>
2023-03-01 22:56:55 +0200

    11

---
+++ session10/main.js
@@ -0,0 +1,37 @@
+
+// console.log("hi console")
+
+// document.write("hi document")
+
+// window.alert("hi window")
+
+// console , document , window
+
+
+// data types 
+// number
+// string 
+// boolean
+// object
+
+
+// primitive , non primitive  data type js  
+
+
+
+
+// var username = "mohamed amr"
+
+// alert("test")
+// alert("mohamed")
+
+// var x = "mohamed";
+// console.log( typeof x )
+
+
+
+// condition ( if , switch )
+// array 
+// loop (for , while , do while)
+
+
---
+++ session11/main.js
@@ -0,0 +1,150 @@
+
+// + , - , * , / , %
+
+// console.log(1+1)
+
+//  < , > , = 
+
+
+// console.log(10 == 2)
+
+
+
+
+// var age = prompt("how old are you ?")
+
+// if (age > 20)
+// {
+//     console.log("hi ")
+// }
+
+
+
+
+// var firstname = prompt("whats your name ?")
+
+// if (firstname == "mohamed"){
+//     console.log("hi mohamed")
+// } else if (firstname == "eslam"){
+//     console.log("hi eslam")
+// }else if (firstname == "ahmed"){
+//     console.log("hi ahmed")
+// }else {
+//     console.log("error")
+// }
+// if(firstname == "mohamed"){
+//     // true
+//     console.log("welcom ya mohamed")
+// }else{
+//     // false
+//     console.log("try again")
+// }
+
+// if(firstname == "ahmed"){
+//     // true
+//     console.log("welcom ya ahmed")
+// }else{
+//     // false
+//     console.log("try again")
+// }
+
+// if(firstname == "eslam"){
+//     // true
+//     console.log("welcom ya eslam")
+// }else{
+//     // false
+//     console.log("try again")
+// }
+
+
+
+
+
+// var oil = prompt("oil ?")
+// var l = prompt("l ?")
+
+// if(oil == 90)
+// {
+//     console.log(l * 20)
+// }else if (oil == 92) 
+// {
+//     console.log(l * 30)
+// }else if (oil == 80)
+// {
+//     console.log(l * 10)
+// }else{
+//     console.log("error")
+// }
+
+
+
+
+// array 
+// var number = 10,20
+// var users = "mohamed"
+
+// var users = [ "ahmed" , "eslam" , "ramy" ,"radwa" , "tamer" ]
+
+// console.log(users.length)
+
+
+// var x = "10";
+// var y = "10";
+// console.log( x === y )
+
+
+// var firstname = prompt("whats your name ?")
+// var users = [ "ahmed" , "mohamed" , "kheder" ]
+
+// if(  users.includes(firstname)  ){
+//     console.log("success")
+// }else{
+//     console.log("name is not exist")
+// }
+
+
+// loop
+
+// var i = 2
+// while(10>i)
+// {
+   
+//     console.log(i)
+//     i = i + 1
+//     // statments
+// }
+
+
+
+
+// var users = [ "tamer" , "eslam" , "mahmoud"  ]
+// var i = 0
+// while(users.length>i)
+// {
+//     console.log(users[i])
+//     i = i +1
+// }
+
+
+
+// 1
+
+
+// 20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
